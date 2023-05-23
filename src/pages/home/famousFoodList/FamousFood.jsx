2e5f450b5baaaeb00ba0220489bbb6f937d6b3a3
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-23 18:43:37 +0600

    completed the favourite food section completely

--- src/pages/home/famousFoodList/FamousFood.jsx
+++ src/pages/home/famousFoodList/FamousFood.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+import "./famous.css";
 import { Pagination } from "swiper";
 import Title from "../../../components/Title";
 import img1 from "../../../assets/home/slide1.jpg";
 import img2 from "../../../assets/home/slide2.jpg";
 import img3 from "../../../assets/home/slide3.jpg";
 import img4 from "../../../assets/home/slide4.jpg";
+import bg from "../../../assets/home/banner.jpg";
 
 const FamousFood = () => {
   return (
@@ -22,7 +24,7 @@ const FamousFood = () => {
         <Swiper
           slidesPerView={4}
           spaceBetween={30}
-          centeredSlides={true}
+          centeredSlides={false}
           pagination={{
             clickable: true,
           }}
@@ -30,26 +32,45 @@ const FamousFood = () => {
           className="mySwiper"
         >
           <SwiperSlide>
-            <img className="h-[350px] w-full" src={img1} alt="" />
+            <img className="" src={img1} alt="" />
             <h1 className="text-3xl font-bold text-center text-white -mt-16">
               Salad
             </h1>
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[350px] w-full" src={img2} alt="" />
+            <img className="" src={img2} alt="" />
             <h1 className="text-3xl font-bold text-center text-white -mt-16">
-             Pizzas
+              Pizzas
             </h1>
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[350px] w-full" src={img3} alt="" />
-            <h1 className="text-3xl font-bold text-center text-white -mt-16">Soups</h1>
+            <img className="" src={img3} alt="" />
+            <h1 className="text-3xl font-bold text-center text-white -mt-16">
+              Soups
+            </h1>
           </SwiperSlide>
           <SwiperSlide>
-            <img className="h-[350px] w-full" src={img4} alt="" />
-            <h1 className="text-3xl font-bold text-center text-white -mt-16">Cake</h1>
+            <img className="" src={img4} alt="" />
+            <h1 className="text-3xl font-bold text-center text-white -mt-16">
+              Cake
+            </h1>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="" src={img1} alt="" />
+            <h1 className="text-3xl font-bold text-center text-white -mt-16">
+              Salad
+            </h1>
           </SwiperSlide>
         </Swiper>
+      </div>
+      <div className="bg flex text-black items-center justify-center flex-col mt-16 h-[450px]">
+        <h1 className="text-3xl font-semibold mb-4">Bistro Boss</h1>
+        <p className="text-center w-1/2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, libero accusamus laborum deserunt ratione dolor
+          officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
+          nihil iusto ducimus incidunt quibusdam nemo.
+        </p>
       </div>
     </div>
   );
