9d8abb97eb03ac256cc87e6ba603c2fe098ec8fe
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-23 17:10:21 +0600

    completed the banner section

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -3,11 +3,18 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Main from '../main/Main';
+import Home from '../pages/home/home/Home';
 
   const router = createBrowserRouter([
     {
       path: "/",
       element:<Main></Main>,
+      children:[
+        {
+          path: "/",
+          element:<Home></Home>,
+        }
+      ]
     },
   ]);
 
--- src/shared/nav/NavBar.jsx
+++ src/shared/nav/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <Navbar fluid={true} rounded={true}>
+      <Navbar className="fixed   opacity-40  z-10 w-full text-white bg-black" fluid={true} rounded={true}>
         <Navbar.Brand
           to="/navbars"
         >
@@ -16,19 +16,19 @@ const NavBar = () => {
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
-          <Link to='/' >
+          <Link className="text-xl font-semibold" to='/' >
             Home
           </Link>
-          <Link to='/' >
+          <Link className="text-xl font-semibold" to='/' >
             Contact Us
           </Link>
-          <Link to='/' >
+          <Link className="text-xl font-semibold" to='/' >
             Dashboard
           </Link>
-          <Link to='/' >
+          <Link className="text-xl font-semibold" to='/' >
             Our Menu
           </Link>
-          <Link to='/' >
+          <Link className="text-xl font-semibold" to='/' >
            Our Shop
           </Link>
         </Navbar.Collapse>
