d494933e52fbaecd47065003c21e4df5efb908e1
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-25 00:30:38 +0600

    added parralex to the background images

--- src/shared/cover/Cover.jsx
+++ src/shared/cover/Cover.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import img1 from "../../assets/menu/banner3.jpg"
+import { Parallax, Background } from "react-parallax";
 
-const Cover = ({img,title,subtitlte}) => {
+const Cover = ({ img, title, subtitlte }) => {
   return (
     <div>
-      <section  style={{ backgroundImage: `url(${img})`, opacity: 0.9 }} className="bg-center  bg-no-repeat  bg-gray-700 bg-blend-multiply">
-        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-          {title}
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-           {subtitlte}
-          </p>
-        </div>
-      </section>
+      <Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="the cover"
+        strength={-200}
+      >
+        <section
+        >
+          <div className="px-4 mx-auto bg-fixed max-w-screen-xl text-center py-24 lg:py-56">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+              {title}
+            </h1>
+            <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+              {subtitlte}
+            </p>
+          </div>
+        </section>
+      </Parallax>
     </div>
   );
 };
--- src/shared/titlebg/TitlewithBg.jsx
+++ src/shared/titlebg/TitlewithBg.jsx
@@ -4,7 +4,7 @@ import "./titlebg.css"
 const TitlewithBg = ({title,text}) => {
   return (
     <div>
-      <div className="bg flex text-black items-center justify-center flex-col mt-16 h-[450px]">
+      <div className="bg flex bg-fixed text-black items-center justify-center flex-col mt-16 h-[450px]">
         <h1 className="text-3xl font-semibold mb-4">{title}</h1>
         <p className="text-center w-1/2">
           {text}
